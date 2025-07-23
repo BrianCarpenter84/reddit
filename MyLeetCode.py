a72bcf01086d399e4cf5cfbc4692f0a75827e750
BrianCarpenter84 <Bcisme84@outlook.com>
2025-07-23 15:50:35 -0400

    Add files via upload

--- MyLeetCode.py
+++ MyLeetCode.py
@@ -1,25 +1,33 @@
 class Solution(object):
     def addTwoNumbers(self, l1, l2):
+        num1 = ''.join(map(str, l1))
+        num2 = ''.join(map(str, l2))
+        new_num = int(num1) + int(num2)
+        total = []
+        for i in str(new_num):
+            total.append(int(i))
+        total.reverse()
+        return total
 
-        def add_lists(l1, l2):
-            num1 = ''.join(map(str, l1))
-            num2 = ''.join(map(str, l2))
-            new_num = int(num1) + int(num2)
-            new_list = []
-            for i in str(new_num):
-                new_list.append(int(i))
-            return new_list
 
-        if len(l1) > len(l2) or len(l2) > len(l1):
-            total = add_lists(l1, l2)
-            total.reverse()
-            return total
+# example :
 
-        if len(l1) == len(l2):
-            return add_lists(l1, l2)
+l1 = [2,4,3]
+l2 = [5,6,4]
 
-l1 = [9,9,9,9,9,9,9]
-l2 = [9,9,9,9]
+# expected Output: [7,0,8]
+#======================================
+# example 2:
+
+# l1 = [0]
+# l2 = [0]
+
+# expected Output: [0]
+#======================================
+# example 3:
+
+# l1 = [9,9,9,9,9,9,9]
+# l2 = [9,9,9,9]
 
 solution = Solution()
 
